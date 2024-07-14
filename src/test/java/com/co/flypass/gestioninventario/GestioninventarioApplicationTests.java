45cc7feb221d009ab3bceda04c5ce436436730ba
fosorio <andres.osorio@f2x.com.co>
2024-07-13 19:12:13 -0500

    Initial commit

---
+++ src/test/java/com/co/flypass/gestioninventario/GestioninventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.flypass.gestioninventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioninventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
